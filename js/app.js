633cac94c547a13eecdafed84535bc041d7f72d4
Udemy course ruby <aposara.acp@gmail.com>
2020-05-11 23:27:23 +0300

    first

---
+++ js/app.js
@@ -0,0 +1,15 @@
+document.addEventListener('click',function() {
+
+const eventshead  = document.querySelector('h1');
+
+   eventshead.style.backgroundColor = 'red';
+});
+
+
+const change_p = document.querySelector('.im-features');
+
+change_p.addEventListener('click', function(){
+
+    console.log("if click will change all p text");
+
+});
